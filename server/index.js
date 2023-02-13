1a1d7ada2155c7fd5e3f44f2c6be25de3ad12586
AC <chenyibo416@gmail.com>
2023-02-13 09:15:59 +0800

    add the env file

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from "./routes/posts.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
@@ -11,18 +14,13 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://chenyibo416:1234@cluster0.vgkpwjj.mongodb.net/TO-DO-LIST?retryWrites=true&w=majority";
-
 const PORT = process.env.PORT || 5000;
 
 mongoose.set("strictQuery", false);
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true })
+  .connect(process.env.CONNECTION_URL, { useNewUrlParser: true })
   .then(() =>
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
   )
   .catch((error) => console.log(error.message));
-
-// mongoose.set("useFindAndModify", false);
